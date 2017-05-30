1114e08631875dc9acd05ed6bb33352299a37e08
Roman <efim1382@gmail.com>
2017-05-30 15:10:34 +0300

    refactore nav map iems

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -6,14 +6,16 @@ import styles from './style.css';
 const Navigation = ({
   items,
 }) => <div className={styles.navigation}>
-  {items.map(item => <Link
-    key={item.link}
-    to={item.link}
-    className={styles.link}
-    {...item.isActive ? { 'data-active': '' } : {}}
-  >
-    { item.name }
-  </Link>)}
+  {items.map(item => (
+    <Link
+      key={item.link}
+      to={item.link}
+      className={styles.link}
+      {...item.isActive ? { 'data-active': '' } : {}}
+    >
+      { item.name }
+    </Link>
+  ))}
 </div>;
 
 Navigation.propTypes = {
