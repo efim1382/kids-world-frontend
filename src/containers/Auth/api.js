d8994af72d91d6fddf7dcf864608fb9d88d080aa
Roman <efim1382@gmail.com>
2017-06-25 18:47:32 +0300

    add auth api

---
+++ src/containers/Auth/api.js
@@ -0,0 +1,17 @@
+import { apiPath } from 'configuration';
+import { createApi } from 'components/Http';
+
+export default createApi({
+  login: {
+    url: `${apiPath}/auth/login/`,
+    options: {
+      method: 'post',
+    },
+  },
+  register: {
+    url: `${apiPath}/auth/register/`,
+    options: {
+      method: 'post',
+    },
+  },
+});
