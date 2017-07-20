5e659a63491d027d46ae1ec8083901d38d72fa6b
Roman <efim1382@gmail.com>
2017-07-20 15:52:16 +0300

    hide user links if logged

--- src/components/Header/UserLinks/index.jsx
+++ src/components/Header/UserLinks/index.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
+import { replace } from 'react-router-redux';
 
 import {
   Popup,
@@ -12,50 +13,78 @@ import { resetToken } from 'containers/Auth/actions';
 
 import styles from './style.css';
 
-const UserLinks = ({ show, dispatch }) => <Popup
-  show={show}
-  className={styles.popup}
->
-  <div className={styles.userLinks}>
-    <Link to="/auth/login">
-      <Button
-        type="transparent"
-        caption="Войти"
-        icon="supervisor_account"
-        className={styles.button}
-      />
-    </Link>
+class UserLinks extends Component {
+  static propTypes = {
+    show: PropTypes.bool.isRequired,
+    dispatch: PropTypes.func.isRequired,
+  };
 
-    <Link to="/auth/register">
-      <Button
-        type="transparent"
-        caption="Зарегистрироваться"
-        icon="person_add"
-        className={styles.button}
-      />
-    </Link>
+  state = {
+    token: false,
+  }
 
-    <Button
-      type="transparent"
-      caption="Выйти"
-      icon="exit_to_app"
-      onClick={() => dispatch(resetToken())}
-      className={styles.button}
-    />
+  componentWillMount() {
+    const item = localStorage.getItem('token');
 
-    <Link to="/profile">
-      <Button
-        type="transparent"
-        caption="Профиль"
-        className={styles.button}
-      />
-    </Link>
-  </div>
-</Popup>;
+    if (!item) {
+      return;
+    }
 
-UserLinks.propTypes = {
-  show: PropTypes.bool.isRequired,
-  dispatch: PropTypes.func.isRequired,
-};
+    this.setState({
+      token: true,
+    });
+  }
+
+  render() {
+    const { show, dispatch } = this.props;
+
+    return (<Popup show={show} className={styles.popup} >
+      <div className={styles.userLinks}>
+        {!this.state.token && <Link to="/auth/login">
+          <Button
+            type="transparent"
+            caption="Войти"
+            icon="supervisor_account"
+            className={styles.button}
+          />
+        </Link>}
+
+        {!this.state.token && <Link to="/auth/register">
+          <Button
+            type="transparent"
+            caption="Зарегистрироваться"
+            icon="person_add"
+            className={styles.button}
+          />
+        </Link>}
+
+        {this.state.token && <Link to="/profile">
+          <Button
+            type="transparent"
+            caption="Профиль"
+            icon="supervisor_account"
+            className={styles.button}
+          />
+        </Link>}
+
+        {this.state.token && <Button
+          type="transparent"
+          caption="Выйти"
+          icon="exit_to_app"
+          onClick={() => {
+            dispatch(resetToken());
+
+            this.setState({
+              token: false,
+            });
+
+            dispatch(replace('/'));
+          }}
+          className={styles.button}
+        />}
+      </div>
+    </Popup>);
+  }
+}
 
 export default connect()(UserLinks);
