1963e85fe2be9a5d9568df7102d1bfec563e8ec8
Roman <efim1382@gmail.com>
2017-12-07 21:41:38 +0300

    markup profile adverts

--- src/containers/Profile/Adverts/index.jsx
+++ src/containers/Profile/Adverts/index.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
 import styles from './style.css';
 
-const Adverts = () => <div className={styles.adverts} />;
+const Adverts = () => <div className={styles.adverts}>
+  <Link to="/">
+    <RaisedButton label="Подать объявление" primary />
+  </Link>
+
+  <ul className={styles.list}>
+    <li className={styles.advert}>
+      <div className={styles.image} style={{ '--image': 'url("/images/ad-image.jpg")' }} />
+
+      <div className={styles.tooltip}>
+        <h4>Детские кроссовки</h4>
+
+        <Link to="/">
+          <FlatButton label="Редактировать" />
+        </Link>
+      </div>
+    </li>
+  </ul>
+</div>;
 
 export default Adverts;
