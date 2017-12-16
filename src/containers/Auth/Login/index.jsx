d98e1641902d74d28ee34169214b513064b48977
Roman <efim1382@gmail.com>
2017-12-17 00:19:54 +0300

    refactore login

--- src/containers/Auth/Login/index.jsx
+++ src/containers/Auth/Login/index.jsx
@@ -1,33 +1,64 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { compose } from 'recompose';
+import { compose, withHandlers } from 'recompose';
 import { connect } from 'react-redux';
 import { replace } from 'react-router-redux';
 
-import { Form, Button } from 'components';
+import { Form, Field, Button } from 'components';
+
+import api from '../api';
+import { setToken } from '../actions';
 
 import styles from './style.css';
 
-class Login extends Component {
-  render() {
-    return (<div className={styles.login}>
-      <span className={styles.title}>Авторизация</span>
+const sendHandler = ({ dispatch }) => (data) => {
+  dispatch(api.actions.login({}, {
+    body: JSON.stringify(data),
+  })).then((response) => {
+    if (response.status !== 200) {
+      return;
+    }
 
-      <Form className={styles.form} model="login" onSubmit={() => {}}>
-        <Button
-          type="submit"
-          appearance="primary"
-          caption="Войти"
-          className={styles.buttonSubmit}
-        />
-      </Form>
-    </div>);
-  }
-}
+    dispatch(setToken(response.token));
+    dispatch(replace('/'));
+  });
+};
+
+const Login = ({ send }) => <div className={styles.login}>
+  <span className={styles.title}>Авторизация</span>
+
+  <Form className={styles.form} model="login" onSubmit={send}>
+    <Field
+      caption="E-Mail"
+      type="email"
+      model=".email"
+      isValidate
+    />
+
+    <Field
+      caption="Пароль"
+      type="password"
+      model=".password"
+      isValidate
+    />
+
+    <Button
+      type="submit"
+      appearance="primary"
+      caption="Войти"
+      className={styles.buttonSubmit}
+    />
+  </Form>
+</div>;
 
 Login.propTypes = {
+  send: PropTypes.func.isRequired,
 };
 
 export default compose(
   connect(),
+
+  withHandlers({
+    send: sendHandler,
+  }),
 )(Login);
