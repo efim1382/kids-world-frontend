2bf1635c8006414a217287d7ee2d3c8c57437b25
Roman <efim1382@gmail.com>
2017-12-21 21:31:04 +0300

    push filters file

---
+++ src/helpers/filters.js
@@ -0,0 +1,15 @@
+import { uploadPath } from 'configuration';
+import categories from 'containers/Profile/Adverts/categories';
+
+export const filterCategories = advertCategory =>
+  categories.filter(category => category.value === advertCategory)[0].name;
+
+export const filterImage = (image) => {
+  if (image === '/images/ad-image.jpg') return `url('${image}')`;
+  return `url(${uploadPath}/${image})`;
+};
+
+export const filterUserPhoto = (image) => {
+  if (image === '/images/user-image.jpg') return `url('${image}')`;
+  return `url(${uploadPath}/${image})`;
+};
