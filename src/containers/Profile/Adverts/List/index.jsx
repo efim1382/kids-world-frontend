0fa5b6a0edf473ced52671b6479d2da982d064dd
Roman <efim1382@gmail.com>
2018-01-13 03:03:27 +0300

    fix lint

--- src/containers/Profile/Adverts/List/index.jsx
+++ src/containers/Profile/Adverts/List/index.jsx
@@ -62,8 +62,8 @@ class List extends Component {
                   deleteAdvert({ id: advert.id }).then(() => {
                     getUserAdverts({ id: this.userId });
                   });
-                }
-              } />
+                }}
+              />
             </div>
           </div>
         </li>)}
